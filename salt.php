e1ac9689e2c40218099858f4906d4e4f393bd126
Your Nan <58438530+Futuree33@users.noreply.github.com>
2021-02-20 01:25:51 +0000

    Update salt.php

--- salt.php
+++ salt.php
@@ -1,1 +1,29 @@
+<?php
 
+class F_Salt
+{
+	function salt_info($info)
+	{
+	   $firstchar = $info[1];
+	   $info = $firstchar.$info .= strlen($info) * pi().$firstchar; 
+	   return $info;
+	}
+
+    function verf_inf($algo, $inf, $hashd_s)
+	{
+		$sa = new F_Salt();
+		if (hash($algo, $sa::salt_info($inf)) == $hashd_s)
+		{
+			unset($sa);
+			return "valid";
+		}
+		else 
+		{
+			unset($sa);
+			return "invalid";
+		}
+		
+	}
+
+}
+?>
