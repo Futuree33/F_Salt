49c6352e37232cf48e8b8f06066d2ce445ecacfe
Your Nan <58438530+Futuree33@users.noreply.github.com>
2021-02-20 01:34:30 +0000

    Update salt.php

--- salt.php
+++ salt.php
@@ -9,7 +9,7 @@ class F_Salt
 	   return $info;
 	}
 
-    function verf_inf($algo, $inf, $hashd_s)
+   	function verf_inf($algo, $inf, $hashd_s)
 	{
 		$sa = new F_Salt();
 		if (hash($algo, $sa::salt_info($inf)) == $hashd_s)
